382ecd4882feb14c7d6d4c2eee28a3758bd56c95
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-19 04:35:26 +0800

    completed frontend - initial commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+	return (
+		<div>
+			<Container>
+				<Row>
+					<Col className="text-center py-3">
+						Copyright &copy; Parrow Eshop
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+}
+
+export default Footer;
